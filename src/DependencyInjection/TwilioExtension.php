669443da206e7e76e5767624c69d8159a0dda260
Vitalii Koreniev <v.korenev@otpusk.com>
2020-05-14 23:06:42 +0300

    created methods to verify and creating verify services automatically using configuration, created and connected yaml config file, updated packages

--- src/DependencyInjection/TwilioExtension.php
+++ src/DependencyInjection/TwilioExtension.php
@@ -10,20 +10,24 @@ declare(strict_types=1);
 namespace Nemishkor\TwilioBundle\DependencyInjection;
 
 
+use Exception;
 use Nemishkor\TwilioBundle\Twilio;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\Routing\Loader\YamlFileLoader;
 use Twilio\Rest\Client;
 
-final class TwilioExtension extends Extension {
+final class TwilioExtension extends Extension implements PrependExtensionInterface {
 
+    /**
+     * @param array $configs
+     * @param ContainerBuilder $container
+     * @throws Exception
+     */
     public function load(array $configs, ContainerBuilder $container) {
-
-        $loader = new YamlFileLoader(new FileLocator(dirname(__DIR__).'/Resources/config/'));
-        $loader->load('twilio.yaml');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
@@ -32,17 +36,25 @@ final class TwilioExtension extends Extension {
         $container
             ->register('nemishkor_twilio_client', Client::class)
             ->addArgument($config['account'])
-            ->addArgument($config['auth_token'])
-            ->addArgument(new Reference('http_client'));
+            ->addArgument($config['auth_token']);
 
         $container
             ->register('nemishkor_twilio', Twilio::class)
             ->setPublic(true)
             ->setAutowired(true)
-            ->addArgument($config['services'])
+            ->addArgument($config['verify']['services'])
             ->addArgument(new Reference('nemishkor_twilio_client'));
 
         $container->setAlias(Twilio::class, 'nemishkor_twilio');
 
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     * @throws Exception
+     */
+    public function prepend(ContainerBuilder $container) {
+        $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
+        $loader->load('twilio.yaml');
     }
 }
--- src/Twilio.php
+++ src/Twilio.php
@@ -10,16 +10,168 @@ declare(strict_types=1);
 namespace Nemishkor\TwilioBundle;
 
 
+use Exception;
+use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
 use Twilio\Rest\Client;
+use Twilio\Rest\Verify\V2\Service\VerificationInstance;
+use Twilio\Rest\Verify\V2\ServiceInstance as VerifyService;
+use UnexpectedValueException;
 
-class Twilio {
+final class Twilio {
 
-    private $servicesConfiguration;
+    private $verifyServicesConfiguration;
     private $client;
+    private $nameConverter;
 
-    public function __construct(array $servicesConfiguration, Client $client) {
-        $this->servicesConfiguration = $servicesConfiguration;
+    public function __construct(array $verifyServicesConfiguration, Client $client) {
+        $this->verifyServicesConfiguration = $verifyServicesConfiguration;
         $this->client = $client;
+        $this->nameConverter = new CamelCaseToSnakeCaseNameConverter(null, true);
+    }
+
+    /**
+     * @param array $config
+     * @return VerifyService
+     * @throws Exception
+     */
+    private function createVerifyServiceFromConfig(array $config): VerifyService {
+
+        if (strlen($config['friendly_name']) > 30) {
+            throw new Exception('Friendly name must contains 30 or less characters');
+        }
+
+        return $this->client->verify->v2->services->create(
+            str_replace(' ', '_', $config['friendly_name']),
+            $this->denormalizeConfig($config)
+        );
+    }
+
+    private function denormalizeConfig(array $config): array {
+        return array_combine(
+            array_map(
+                function ($k) {
+                    return $this->nameConverter->denormalize((string)$k);
+                },
+                array_keys($config)
+            ),
+            array_map(
+                function ($v) {
+                    switch (true) {
+                        case is_string($v):
+                            return $this->nameConverter->denormalize($v);
+                            break;
+                        case is_array($v):
+                            return $this->denormalizeConfig($v);
+                            break;
+                    }
+
+                    return $v;
+                },
+                array_values($config)
+            )
+        );
+    }
+
+    /**
+     * @return VerifyService
+     * @throws Exception
+     */
+    private function getFirstService(): VerifyService {
+
+        $services = $this->client->verify->v2->services->read();
+
+        if (count($services) === 1) {
+            return array_values($services)[0];
+        }
+
+        if (count($this->verifyServicesConfiguration) === 0) {
+            throw new UnexpectedValueException(
+                sprintf(
+                    'Not found any verify services in configuration. Add service declaration to file "config/packages/twilio.yaml"'
+                )
+            );
+        }
+
+        return $this->createVerifyServiceFromConfig(array_values($this->verifyServicesConfiguration)[0]);
+    }
+
+    /**
+     * @param string $name
+     * @return VerifyService
+     * @throws Exception
+     */
+    private function createVerifyServiceByName(string $name): VerifyService {
+
+        $configurations = array_filter(
+            $this->verifyServicesConfiguration,
+            static function (array $config) use ($name): bool {
+                return $config['friendly_name'] === $name;
+            }
+        );
+
+        if (count($configurations) !== 1) {
+            throw new UnexpectedValueException(
+                sprintf(
+                    'Expected 1 verify service configurations with friendly name "%s" but found %s',
+                    $name,
+                    count($configurations)
+                )
+            );
+        }
+
+        return $this->createVerifyServiceFromConfig(array_values($configurations)[0]);
+    }
+
+    /**
+     * @param string $serviceId
+     * @return VerifyService
+     * @throws Exception
+     */
+    private function getVerifyService(string $serviceId) {
+
+        $services = $this->client->verify->v2->services->read();
+
+        $services = array_filter(
+            $services,
+            static function (VerifyService $service) use ($serviceId): bool {
+                return $serviceId === $service->friendlyName || $serviceId === $service->sid;
+            }
+        );
+        if (count($services) > 1) {
+            throw new UnexpectedValueException(
+                sprintf(
+                    'Expected less then 2 verify services with name "%s" but found %s',
+                    $serviceId,
+                    count($services)
+                )
+            );
+        }
+
+        if (count($services) === 1) {
+            return array_values($services)[0];
+        }
+
+        return $this->createVerifyServiceByName($serviceId);
+    }
+
+    /**
+     * @param string $to
+     * @param string $channel
+     * @param array $options
+     * @param string|null $service - friendly name or sid
+     * @return VerificationInstance
+     * @throws Exception
+     */
+    public function verify(
+        string $to,
+        string $channel,
+        array $options = [],
+        ?string $service = null
+    ): VerificationInstance {
+        return $this->client->verify->v2
+            ->services($service === null ? $this->getFirstService()->sid : $this->getVerifyService($service)->sid)
+            ->verifications
+            ->create($to, $channel, $options);
     }
 
 }
